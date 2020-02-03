0eb16f6c10fb1f2a636a8d62c8df911a853b6eef
Xio <vinicius.jovemaprendiz.fya@gmail.com>
2020-02-03 14:23:15 -0300

    Adicionando os arquivos do projeto.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	require_once('navbar.html')
+?>
+
+<!DOCTYPE html>
+<!-- NAVBAR da aplicação -->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body style="font-family: Courier New">
+
+
+		<h1 style="padding-top: 100px">ATIVIDADES</h1>
+		<div class="p-5 bg-warning border border-primary rounded" style="margin-top: 5p0x; margin-left: 500px; margin-right: 500px">
+			<h1 st></h1>
+			<form>
+				<input type="text" name="email" class="border rounded">
+				<input type="text" name="senha" class="border rounded">
+				<span class='p-3'>
+					<button class="btn btn-success" text="Login">
+						<?php
+
+						?>
+					</button>
+				</span>
+			</form>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="bootstrap/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
